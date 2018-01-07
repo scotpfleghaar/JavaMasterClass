8d88b2d02d4d5fba7403bf8c3bae2cf8b390dd2e
Scot <scot.pfleghaar@gmail.com>
2018-01-07 17:43:24 -0600

    Initial Commit

---
+++ Account.java
@@ -0,0 +1,9 @@
+public class Account {
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerEmailAddress;
+    private String customerPhoneNumber;
+
+    //Getters and Setters
+}
